4797c60caa8f7d7c5bf621539b722b3e056f4584
Athlos <pascalcross15@gmail.com>
2015-05-06 11:28:54 +1200

    Readme and database
    Database SQL file added, readme to use it added

---
+++ Database.sql
@@ -0,0 +1,34 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.2.0.4947
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for test
+CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `test`;
+
+
+-- Dumping structure for table test.paper
+CREATE TABLE IF NOT EXISTS `paper` (
+  `Paper_Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Submitted_by` varchar(255) NOT NULL,
+  `Paper_URL` varchar(255) NOT NULL,
+  `Metadata` varchar(255) DEFAULT NULL,
+  `Date_of_publication` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Paper_Id`),
+  UNIQUE KEY `Paper_Id` (`Paper_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table test.paper: ~0 rows (approximately)
+/*!40000 ALTER TABLE `paper` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paper` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+TO INSTALL DATABASE
+please install heidiSQL
+open it and make a new connection called "SE" with default settings (which is local host)
+you want to click file > load SQL file and select the file in your github folder
+it will create an SQL query to create the database, click the blue arrow or f9 to execute it and create the database
+it has no data, just keys
+use it to test input and output functionality
