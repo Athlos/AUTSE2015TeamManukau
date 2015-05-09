c007174bdb55d28da622534b2759be3517adeda4
Mark Manson <mark.manson.uni@gmail.com>
2015-05-09 20:07:21 +1200

    Added tables for meta data also working on fixes
    problem with sql query trying to get it to work

--- Papermetadataform/addpapermetadata.php
+++ Papermetadataform/addpapermetadata.php
@@ -13,79 +13,80 @@
 			{
 				$methodology_name = $_GET["methodology_name"];
 			} 
-			else $methodology_name = $_GET["null"];
+			else $methodology_name = NULL;
 			if(isset ($_GET["methodology_description"]))
 			{
 				$methodology_description = $_GET["methodology_description"];
 			}
-			else $methodology_description = $_GET["null"];
+			else $methodology_description = NULL;
 			if(isset ($_GET["method_name"]))
 			{
 				$method_name = $_GET["method_name"];
 			}
-			else $method_name = $_GET["null"];
+			else $method_name = NULL;
 			if(isset ($_GET["method_description"]))
 			{
 				$method_description = $_GET["method_description"];
 			}
-			else $method_description = $_GET["null"];
+			else $method_description = NULL;
 			if(isset ($_GET["biblography_ref"]))
 			{
 				$biblography_ref = $_GET["biblography_ref"];
 			}
-			else $biblography_ref = $_GET["null"];
+			else $biblography_ref = NULL;
 			if(isset ($_GET["research_level"]))
 			{
 				$research_level = $_GET["research_level"];
 			}
-			else $research_level = $_GET["null"];
+			else $research_level = NULL;
 			if(isset ($_GET["research_question"]))
 			{
 				$research_question = $_GET["research_question"];
 			}
-			else $research_question = $_GET["null"];
+			else $research_question = NULL;
 			if(isset ($_GET["research_method"]))
 			{
 				$research_method = $_GET["research_method"];
 			}
-			else $research_method = $_GET["null"];
+			else $research_method = NULL;
 			if(isset ($_GET["research_metrics"]))
 			{
 				$research_metrics = $_GET["research_metrics"];
 			}
-			else $research_metrics = $_GET["null"];
+			else $research_metrics = NULL;
 			if(isset ($_GET["research_participants"]))
 			{
 				$research_participants = $_GET["research_participants"];
 			}
-			else $research_participants = $_GET["null"];
+			else $research_participants = NULL;
 			if(isset ($_GET["evidence_context"]))
 			{
 				$evidence_context = $_GET["evidence_context"];
 			}
-			else $evidence_context = $_GET["null"];
+			else $evidence_context = NULL;
 			if(isset ($_GET["benefit_outcome"]))
 			{
 				$benefit_outcome = $_GET["benefit_outcome"];
 			}
-			else $benefit_outcome = $_GET["null"];
+			else $benefit_outcome = NULL;
 			if(isset ($_GET["result"]))
 			{
 				$result = $_GET["result"];
 			}
-			else $result = $_GET["null"];
+			else $result = NULL;
 			if(isset ($_GET["method_implementation_integrity"]))
 			{
 				$method_implementation_integrity = $_GET["method_implementation_integrity"];
 			}
-			else $method_implementation_integrity = $_GET["null"];
+			else $method_implementation_integrity = NULL;
 			
 			//connect to the database to submit data
-			$conn = @mysqli_connect('127.0.0.1','root','', 'test')
+			$conn = @mysqli_connect('127.0.0.1','root','','test')
 			or die('Failed to connect to server');
 			
 			//creates a query that submits the data into the table names in the table are subject to change
-			$query = "INSERT INTO papers_metadata(
+			$query = "INSERT INTO paper_metadata(
+			Submission_Date,
 			methodology_name,
 			methodology_description,
 			method_name,
@@ -101,6 +102,7 @@
 			result,
 			method_implementation_integrity)
 			VALUES (
+			now()
 			'$methodology_name',
 			'$methodology_description',
 			'$method_name',
@@ -122,8 +124,11 @@
 			{
 				echo "Paper meta-data successfully added to the database";
 			}
-			else echo "Paper meta-data failed to add to the database"
-			
+			else 
+			{
+				//echo "Paper meta-data failed to add to the database\n";
+				echo mysql_errno($conn) . ": " . mysql_error($conn) . "\n";
+			}
 			//close connection
 			mysqli_close($conn);
 		?>
--- Papermetadataform/papermetadataform.php
+++ Papermetadataform/papermetadataform.php
@@ -6,47 +6,47 @@
 	<body>
 		<h1>Paper Meta-data Form</h1>
 		<!--This is a form that multiple areas to fill out for paper meta data-->
-		<form action = "AddPapermetadata.php" method = "GET">
-			Methodology Name:<br>
+		<form action = "AddPapermetadata.php" method = "GET">			
+			Methodology Name:
 			<input type="text" name="methodology_name">
 			<br>
-			Methodology Description:<br>
+			Methodology Description:
 			<input type="text" name="methodology_description">
 			<br>
-			Method Name:<br>
+			Method Name:
 			<input type="text" name="method_name">
 			<br>
-			Method description:<br>
+			Method description:
 			<input type="text" name="method_description">
 			<br>
-			Evidence Source Bibliography References:<br>
+			Evidence Source Bibliography References:
 			<input type="text" name="bibliography_ref">
 			<br>
-			Evidence Source Research Revel:<br>
+			Evidence Source Research Level:
 			<input type="text" name="research_level">
 			<br>
-			Research Question:<br>
+			Research Question:
 			<input type="text" name="research_question">
 			<br>
-			Research Method:<br>
+			Research Method:
 			<input type="text" name="research_method">
 			<br>
-			Research Metrics:<br>
+			Research Metrics:
 			<input type="text" name="research_metrics">
 			<br>
-			Research Participants:<br>
+			Research Participants:
 			<input type="text" name="research_participants">
 			<br>
-			Evidence context (what, why, when, where, who, how):<br>
+			Evidence context (what, why, when, where, who, how):
 			<input type="text" name="evidence_context">
 			<br>
-			Evidence context benefit/outcome under test:<br>
+			Evidence context benefit/outcome under test:
 			<input type="text" name="benefit_outcome">
 			<br>
-			Evidence context Result:<br>
+			Evidence context Result:
 			<input type="text" name="result">
 			<br>
-			Evidence context method implementation integrity:<br>
+			Evidence context method implementation integrity:
 			<input type="text" name="method_implementation_integrity">
 			<br>
 			<input type="submit" value="Submit">
