d39aa30c81c163be25ddb54eb8a465121ed2d06c
Pascal Cross <pascalcross15@gmail.com>
2015-06-06 17:59:18 +1200

    Remodeled Searching
    Searching function is how Jim requested, data is transferred but the
    Query is not executed yet

--- Searching/AdvancedSearchForm.php
+++ Searching/AdvancedSearchForm.php
@@ -2,52 +2,130 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>Search Paper</title>
+
+<style>
+table, th, td {
+
+    border-width:5px;	
+    border-style:groove;
+	text-align:center;
+}
+</style>
+
+<script type="text/javascript">
+
+	var counter = 0;
+		
+		function cloneRow() {
+			counter++;
+			
+			var andor = document.getElementById("andor");
+			var cloneandor = andor.cloneNode(true);
+			cloneandor.name = "andor"+counter;
+			
+			var field = document.getElementById("searchField");
+			var clonefield = field.cloneNode(true);
+			clonefield.name = "field"+counter;
+			
+			var operator = document.getElementById("operator");
+			var cloneoperator = operator.cloneNode(true);
+			cloneoperator.name = "operator"+counter;
+			
+			var value = document.getElementById("value");
+			var clonevalue = value.cloneNode(true);
+			clonevalue.name = "value"+counter;
+			
+			// Find a <table> element with id="myTable":
+			var table = document.getElementById("searchTable");
+
+			// Create an empty <tr> element and add it to the 1st position of the table:
+			var row = table.insertRow(counter);
+			
+			// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+			var cell1 = row.insertCell(0);
+			var cell2 = row.insertCell(1);
+			var cell3 = row.insertCell(2);
+			var cell4 = row.insertCell(3);
+			
+			cell1.appendChild(cloneandor);
+			cell2.appendChild(clonefield);
+			cell3.appendChild(cloneoperator);
+			cell4.appendChild(clonevalue);
+
+		}
+	</script>
+
 </head>
 <body>
+<label value="field" id="test">  </label>
+
 <form action="SearchForm.php">
     <input type="submit" value="<- Back">
 	</form>
-	<h1>AdvancedSearch</h1>
-	<form action = "Search.php" method = "GET" >
-	<label>Search : <input type = "text" name = "search"> </label><br>
+	
+	<form action = "Search.php" method = "GET" id="searchForm" >
+	<br><label>Search : <input type = "text" name = "search"> </label><br>
 	<br><br>
-	
+	<h1>AdvancedSearch</h1>
 	<input type="hidden" name="searchType" value="Advanced">
+
 	
-	<label>Methodology :   </label>
-	<select name="methodology">
-	<!--This is hardcoded for now, should make it pull methodologies from the tables to select -->
-	<option></option>
-	<option>Scrum</option>
-	<option>XP</option>
-	<option>Kanban</option>
-	</select><p>
+	<table id="searchTable">
+	<tr>
+		<td>Combination</td>
+		<td><label>Field   </label></td>
+		<td><label>Operator   </label></td>
+		<td><label>Value </label></td>
+	</tr>
 	
-	<label>Methods Used :   </label>
-	<select name = "method">
-	<!--This is hardcoded for now, should make it pull methods from the tables to select -->
-	<option></option>
-			<option>Method A</option>
-			<option>Method B</option>
-			<option>Method C</option>
-	</select><p>
 	
-	<!-- does not work, removed for now
-	<label>Group By :   </label>
-	<select name="group">
-	<option></option>
-	<option value="paper_methodology_and_method.paper_methodology_name">Methodology</option>
-	<option value="paper_methodology_and_method.paper_method_name">Methods</option>
-	</select><p>
-	-->
-	<label>Sort By :   </label>
-	<select name="sort">
-	<option></option>
-	<option>Name</option>
-	</select><p>
+  <tr id ="searchRow">
+    <td>
+		<select name = "andor" id="andor">
+			<option> And </option>
+			<option> Or </option>
+		</select>
+	</td>
+  
+    <td>
+		<select name="field" id="searchField">
+			<option>Year of Publication</option>
+			<option>Author</option>
+			<option>Journal of Publication</option>
+			<option>Methodology</option>
+			<option>Practice</option>
+			<option>Research Level</option>
+			<option>Evidence Context<To include who what etc></option>
+			<option>Research Question</option>
+			<option>Research Method</option>
+			<option>Research Metrics</option>
+			<option>Research participants</option>
+			<option>Credibility Rating</option>
+			<option>Quality Rating</option>
+		</select>
+	</td>
+	
+    <td>
+		
+		<select name = "operator" id="operator">
+			<option> = </option>
+			<option> > </option>
+			<option> < </option>
+		</select>
+	</td> 
+	
+    <td>
+		<label><input type = "text" name = "value" id="value"> </label>
+	</td>
+	
+  </tr>
+</table>
+
+<br><input type="button" onclick="cloneRow()" value="Add Search parameter" /><br>
 
 	<br><input type = "submit" value = "Search">
 	</form>
+
 
 </body>
 </html> 
--- Searching/Search.php
+++ Searching/Search.php
@@ -7,31 +7,69 @@
 <body>
 	<?php
 	$AdvancedSearch = false;
-	if ($_GET["searchType"] == "Advanced"){
-		$AdvancedSearch = true;
-		echo $_GET["search"];
-			if (isset ($_GET["methodology"])){
-			$methodology =  $_GET["methodology"];
-		} else echo "methodology Is Incorrect Data Type"."<br>";
+
+	$count = "";
+	$andors = array();
+	while (isset ($_GET["andor".$count])){
+		array_push($andors, $_GET["andor".$count]);
 		
-		if (isset ($_GET["method"])){
-			$method = $_GET["method"];
-		} else echo "method Is Incorrect Data Type"."<br>";
-		
-		if (isset ($_GET["group"])){
-			$group = $_GET["group"];
-		} else $group = "";
-		
-		if (isset ($_GET["sort"])){
-			$sort = $_GET["sort"];
-		} else echo "sort Is Incorrect Data Type"."<br>";
-	} 
+		if($count == ""){
+			$count = 1;
+		} else {
+			$count++;
+		}
+	}
+	echo "<br>"."And/Or ";
+	print_r($andors);
 	
 	
+	$count = "";
+	$fields = array();
+	while (isset ($_GET["field".$count])){
+		array_push($fields, $_GET["field".$count]);
+		
+		if($count == ""){
+			$count = 1;
+		} else {
+			$count++;
+		}
+	}
+	echo "<br>"."Fields ";
+	print_r($fields);
+	
+	$count = "";
+	$operators = array();
+	while (isset ($_GET["operator".$count])){
+		array_push($operators, $_GET["operator".$count]);
+		
+		if($count == ""){
+			$count = 1;
+		} else {
+			$count++;
+		}
+	}
+	echo "<br>"."Operators ";
+	print_r($operators);
+	
+	$count = "";
+	$values = array();
+	while (isset ($_GET["value".$count])){
+		array_push($values, $_GET["value".$count]);
+		if($count == ""){
+			$count = 1;
+		} else {
+			$count++;
+		}
+	}
+	echo "<br>"."Values ";
+	print_r($values);
+	
+	
+	
+ 	
 	//First, we get the values out of AddPaperForm.php, which are the URL and Submitter
 		if (isset ($_GET["search"])){
 			$search = $_GET["search"];
-			//echo "|".$search."|";
 		} else echo "link Is Incorrect Data Type"."<br />";
 	
 	
@@ -47,48 +85,21 @@
 	//paper_rating 15-23
 	//paper_research 24-28
 	
+	$parameters = "";
 	
-	
-	
-	
-	
-	
-		if($AdvancedSearch && $methodology != "") {
-			$methodologyQuery = " WHERE paper_methodology_and_method.paper_methodology_name='$methodology'";
-		} else $methodologyQuery = "";
+	while($count > 0) {
+		$count--;
+		//$parameters = $parameters . "";
+		if($andors[$count] == "And") {
+			$parameters = $parameters . " AND WHERE '$fields[$count]' = '$values[$count]'";
+		} else {
+			$parameters = $parameters . " OR WHERE '$fields[$count]' = '$values[$count]'";
+		}
 		
-		if($AdvancedSearch && $method != "") {
-			if($methodology = "")
-				$methodologyQuery = $methodologyQuery . " WHERE paper_methodology_and_method.paper_method_name='$method'";
-			else
-				$methodologyQuery = $methodologyQuery . " AND paper_methodology_and_method.paper_method_name='$method'";
-		} 
-		
-			if($methodology = "")
-				$methodologyQuery = $methodologyQuery . " WHERE approved_papers.paper_name LIKE '%$search%'";
-			else
-				$methodologyQuery = $methodologyQuery . " AND approved_papers.paper_name LIKE '%$search%'";
-		
-		if($AdvancedSearch && $group != "") {
-			$methodologyQuery = $methodologyQuery . " GROUP BY '$group'";
-		} 
-		$order = "";
-		if($AdvancedSearch && $sort != "") {
-			//echo "|".$sort."|";
-			if($sort == "Name")
-				$methodologyQuery = $methodologyQuery . " ORDER BY  approved_papers.paper_name";
-			if($sort == "Confidence")
-				$methodologyQuery = $methodologyQuery . " ORDER BY paper_rating.paper_average_confidence";
-			if($sort == "Credibility") {
-				$methodologyQuery = $methodologyQuery . " ORDER BY approved_papers.paper_name";
-				echo "|".$sort."|";
-				}
-		} 
-		//else echo "stuff";
-		
+	} 
 
 		$select = "SELECT * 
-		FROM approved_papers";
+		FROM approved_papers ";
 		$join = " INNER JOIN paper_evidence_source_and_item 
 		ON approved_papers.paper_name=paper_evidence_source_and_item.paper_name_evidence
 		INNER JOIN paper_methodology_and_method 
@@ -98,14 +109,13 @@
 		INNER JOIN paper_research
 		ON approved_papers.paper_name=paper_research.paper_name_research
 		INNER JOIN paper_bibliography_info
-		ON approved_papers.paper_name=paper_bibliography_info.paper_name_bibliography";
+		ON approved_papers.paper_name=paper_bibliography_info.paper_name_bibliography
+		WHERE approved_papers.paper_name LIKE '%$search%'";
 		
 		
-		$query = $select . $join . $methodologyQuery . ";";
+		$query = $select . $join . $parameters . ";";
+		echo "<br>"."<br>"."EXECUTING QUERY : ".$query;
 		$savedQuery = $query;
-		//echo $query;
-		//echo $methodologyQuery;
-		//echo $savedQuery;
 		$res = $conn->query($query);
 	//show results 
 	$array = array();
@@ -115,14 +125,7 @@ if($res != false) {
 	or die("No Results Found");
 
 	echo "<table width='100%' border='1'>";
-	echo "<tr><th>Paper Name</th><th>Paper Information</th><th>Bibliography Info</th><th>Ratings</th><th>Research Information</th></tr>";
-
-	//<th>Bibliography References</th>
-	//<th>Research Level</th><th>Evidence Context</th><th>Benefits</th>
-	//<th>Result</th><th>Implementation Integrity</th>
-	
-	
-	
+	echo "<tr><th>Paper Name</th><th>Paper Information</th><th>Bibliography Info</th><th>Ratings</th><th>Research Information</th></tr>";	
 	
 	while ($row) {
 	
@@ -220,60 +223,12 @@ if($res != false) {
 	$row = mysqli_fetch_row($res);
 
 	} echo "</table>";
-} else echo "No Results Found";
-/* 			
-		//This is the query string, holding the SQL command to use
-		//now() inputs the current date and time into the table
-		
-		$query = "SELECT * FROM approved_papers WHERE paper_name LIKE '%$search%'";
-		
-		$result = $conn->query($query);
-		
-	//show results 
-	
-	$row = mysqli_fetch_row($result);
-	
-	$intNumButton = 0;
-    // output data of each row
-	
-	echo "<table width='100%' border='1'>";
-	echo "<tr><th>Entries</th></tr>";
-	
-    while($row) {
-        echo $row[0];
-		echo "<tr><td>";
-		?>
+} else echo "<br>"."No Results Found";
 
-		<!--This form goes to the rate the paper (confidence/quality process) -->
-		<form action = "paperDisplay.php" method = "GET" >
-		
-		<!--hidden input field attached to every entry that contains the name of the paper so that the next form knows which button was clicked. -->
-		<input type="hidden" name="nameOfPaper" value=<?php echo $row[0]?>> 
-		
-		
-		<label><?php echo $row[0]?><name = "display"> </label><br>
-		<label>Click <name = "display"> </label>
-		<a href="<?php echo $row[3]?>">Here</a><br>
-		<label>Methodology : <name = "display"> </label><br>
-		<label>Bibliography : <name = "display"> </label><br>
-		<label>Research Question : <name = "display"> </label><br>
-		<label>Credibility Rating : <name = "display"> </label><br>
-		<label>Quality Rating: <name = "display"> </label><br>
-		
-		<input type = "submit" name = "goButton" value = "Go to">
-		
-		<!-- http://stackoverflow.com/questions/2680160/how-can-i-tell-which-button-was-clicked-in-a-php-form-submit SEND THIS TO A FORM THAT REDIRECTS TO EITHER FAVOURITE OR CONFIDENCE/QUALITY DEPENDING ON WHICH BUTTON WAS
-		CLICKED-->
-		</form>
-		
-		<?php
-		echo "</td></tr>";
-		$row = mysqli_fetch_row($result);
-    } echo "</table>"; */
 		//now we close the database
 		mysqli_close($conn);
 		
-		
+
 		
 		if($AdvancedSearch) {
 	?> <br><a href="AdvancedSearchForm.php">Go Back</a><br> <?php
